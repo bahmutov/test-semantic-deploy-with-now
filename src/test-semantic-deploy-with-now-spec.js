2d035db0541c344044389509a2148b0b1888d712
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-08-23 23:26:09 -0400

    initial

---
+++ src/test-semantic-deploy-with-now-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const testSemanticDeployWithNow = require('.')
+
+describe('test-semantic-deploy-with-now', () => {
+  it('write this test', () => {
+    console.assert(testSemanticDeployWithNow, 'should export something')
+  })
+})
